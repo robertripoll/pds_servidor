c17ad84830f57a95aa617d192714e15f6140d39e
daniiel_re <u1933604@campus.udg.edu>
2018-03-14 10:04:11 +0100

    Push Usuari Model
    · Atributs numCompres, numVendes i numValoracions no posats ja que es pot saber amb el size de les estructures compres, vendes i valoracions

--- src/main/java/org/udg/pds/cheapy/model/User.java
+++ src/main/java/org/udg/pds/cheapy/model/User.java
@@ -2,99 +2,278 @@ package org.udg.pds.cheapy.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.joda.time.DateTime;
 
 import javax.persistence.*;
+import javax.swing.text.View;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-@Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"email", "username"}))
+@Entity(name = "usuaris")
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"correu", "nom"}))
 public class User implements Serializable {
-  /**
-   * Default value included to remove warning. Remove or modify at will. *
-   */
-  private static final long serialVersionUID = 1L;
+    /**
+     * Default value included to remove warning. Remove or modify at will. *
+     */
+    private static final long serialVersionUID = 1L;
 
-  public User() {
-  }
+    public User(){
+    }
 
-  public User(String username, String email, String password) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.tasks = new ArrayList<>();
-  }
+    public User(String sexe, String nom, String cognoms, String telefon, DateTime dataNaix, String correu, String contrasenya, Ubicacio ubicacio){
+        this.sexe = sexe;
+        this.nom = nom;
+        this.cognoms = cognoms;
+        this.telefon = telefon;
+        this.dataNaix = dataNaix;
+        this.correu = correu;
+        this.contrasenya = contrasenya;
+        this.ubicacio = ubicacio;
+        this.missatges = new ArrayList<>();
+        this.converses = new ArrayList<>();
+        this.valoracions = new ArrayList<>();
+        this.compres = new ArrayList<>();
+        this.vendes = new ArrayList<>();
+        this.favorits = new ArrayList<>();
+        this.prodVenda = new ArrayList<>();
+    }
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @JsonView(Views.Private.class)
-  protected Long id;
+    //-------------------- ATRIBUTS DE LA CLASSE --------------------//
 
-  @NotNull
-  @JsonView(Views.Public.class)
-  private String username;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(Views.Private.class)
+    protected Long id;
 
-  @NotNull
-  @JsonView(Views.Private.class)
-  private String email;
+    @NotNull
+    @JsonIgnore
+    private String contrasenya;
 
-  @NotNull
-  @JsonIgnore
-  private String password;
+    @NotNull
+    @JsonView(Views.Private.class)
+    private String correu;
 
-  @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-  @JsonView(Views.Complete.class)
-  private Collection<Task> tasks;
+    @NotNull
+    @JsonView(Views.Public.class)
+    private String nom;
 
-  public Long getId() {
-    return id;
-  }
+    @NotNull
+    @JsonView(Views.Private.class)
+    private String cognoms;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    @NotNull
+    @JsonView(Views.Public.class)
+    private String sexe;
 
-  public String getPassword() {
-    return password;
-  }
+    @NotNull
+    @JsonView(Views.Private.class)
+    private String telefon;
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    @NotNull
+    @JsonView(Views.Private.class)
+    private DateTime dataNaix;
 
-  public String getUsername() {
-    return username;
-  }
+    @NotNull
+    @JsonView(Views.Private.class)
+    private Ubicacio ubicacio;
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    //-------------------- ATRIBUTS AMB RELACIÓ AMB ALTRES ENTITATS --------------------//
 
-  public String getEmail() {
-    return email;
-  }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Missatge> missatges;
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Conversacio> converses;
 
-  public Collection<Task> getTasks() {
-    // Since tasks is collection controlled by JPA, it has LAZY loading by default. That means
-    // that you have to query the object (calling size(), for example) to get the list initialized
-    // More: http://www.javabeat.net/jpa-lazy-eager-loading/
-    tasks.size();
-    return tasks;
-  }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Valoracio> valoracions;
 
-  public void setTasks(List<Task> ts) {
-    this.tasks = ts;
-  }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Transaccio> compres;
 
-  public void addTask(Task task) {
-    tasks.add(task);
-  }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Transaccio> vendes;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Producte> favorits;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonView(Views.Complete.class)
+    private Collection<Producte> prodVenda;
+
+    //-------------------- GETTERS I SETTERS --------------------//
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getContrasenya(){
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya){
+        this.contrasenya = contrasenya;
+    }
+
+    public String getCorreu(){
+        return correu;
+    }
+
+    public void setCorreu(String correu){
+        this.correu = correu;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognoms(){
+        return cognoms;
+    }
+
+    public void setCognoms(String cognoms){
+        this.cognoms = cognoms;
+    }
+
+    public String getSexe(){
+        return sexe;
+    }
+
+    public void setSexe(String sexe){
+        this.sexe = sexe;
+    }
+
+    public String getTelefon(){
+        return telefon;
+    }
+
+    public void setTelefon(String telefon){
+        this.telefon = telefon;
+    }
+
+    public DateTime getDataNaix(){
+        return dataNaix;
+    }
+
+    public void setDataNaix(DateTime dataNaix){
+        this.dataNaix = dataNaix;
+    }
+
+    public Ubicacio getUbicacio() {
+        return ubicacio;
+    }
+
+    public void setUbicacio(Ubicacio ubicacio){
+        this.ubicacio = ubicacio;
+    }
+
+    //-------------------- OPERACIONS AMB LES COLECCIONS --------------------//
+
+    public Collection<Missatge> getMissatges(){
+        missatges.size();
+        return missatges;
+    }
+
+    public void setMissatges(List<Missatge> miss){
+        this.missatges = miss;
+    }
+
+    public void addMissatge(Missatge missatge){
+        missatges.add(missatge);
+    }
+
+    public Collection<Conversacio> getConverses(){
+        converses.size();
+        return converses;
+    }
+
+    public void setConverses(List<Conversacio> cv){
+        this.converses = cv;
+    }
+
+    public void addConversacio(Conversacio conversacio){
+        converses.add(conversacio);
+    }
+
+    public Collection<Valoracio> getValoracions(){
+        valoracions.size();
+        return valoracions;
+    }
+
+    public void setValoracions(List<Valoracio> val){
+        this.valoracions = val;
+    }
+
+    public void addValoracio(Valoracio valoracio){
+        valoracions.add(valoracio);
+    }
+
+    public Collection<Transaccio> getCompres(){
+        compres.size();
+        return compres;
+    }
+
+    public void setCompres(List<Transaccio> comp){
+        this.compres = comp;
+    }
+
+    public void addCompra(Transaccio compra){
+        compres.add(compra);
+    }
+
+    public Collection<Transaccio> getVendes(){
+        vendes.size();
+        return vendes;
+    }
+
+    public void setVendes(List<Transaccio> vend){
+        this.vendes = vend;
+    }
+
+    public void addVenda(Transaccio venda){
+        vendes.add(venda);
+    }
+
+    public Collection<Producte> getFavorits(){
+        favorits.size();
+        return favorits;
+    }
+
+    public void setFavorits(List<Producte> fav){
+        this.favorits = fav;
+    }
+
+    public void addFavorit(Producte prod){
+        favorits.add(prod);
+    }
+
+    public Collection<Producte> getProdVenda(){
+        prodVenda.size();
+        return prodVenda;
+    }
+
+    public void setProdVenda(List<Producte> prodV){
+        this.prodVenda = prodV;
+    }
+
+    public void addProdVenda(Producte prod){
+        prodVenda.add(prod);
+    }
 }
