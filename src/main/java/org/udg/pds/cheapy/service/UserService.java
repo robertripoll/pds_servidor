3dc0c41420fe58269e017b998cc1e7f6ee954438
daniiel_re <u1933604@campus.udg.edu>
2018-03-14 18:55:51 +0100

    Add files via upload

--- src/main/java/org/udg/pds/cheapy/service/UserService.java
+++ src/main/java/org/udg/pds/cheapy/service/UserService.java
@@ -17,9 +17,9 @@ public class UserService {
   @PersistenceContext
   private EntityManager em;
 
-  public User matchPassword(String username, String password) {
-    Query q = em.createQuery("select u from User u where u.username=:username");
-    q.setParameter("username", username);
+  public User matchPassword(String correu, String contrasenya) {
+    Query q = em.createQuery("select u from User u where u.correu=:correu");
+    q.setParameter("correu", correu);
 
     User u;
     try {
@@ -28,31 +28,31 @@ public class UserService {
       throw new EJBException("No user with this username exists");
     }
 
-    if (u.getPassword().equals(password))
+    if (u.getContrasenya().equals(contrasenya))
       return u;
     else
       throw new EJBException("Password does not match");
   }
 
-  public User register(String username, String email, String password) {
+  public User register(String nom, String correu, String contrasenya) {
 
-    Query q = em.createQuery("select u from User u where u.email=:email");
-    q.setParameter("email", email);
+    Query q = em.createQuery("select u from User u where u.correu=:correu");
+    q.setParameter("correu", correu);
     try {
       User u = (User) q.getSingleResult();
     } catch (Exception e) {
       throw new EJBException("Email already exist");
     }
 
-    q = em.createQuery("select u from User u where u.username=:username");
-    q.setParameter("username", username);
+    q = em.createQuery("select u from User u where u.nom=:nom");
+    q.setParameter("nom", nom);
     try {
       User u = (User) q.getSingleResult();
     } catch (Exception e) {
       throw new EJBException("Username already exists");
     }
 
-    User nu = new User(username, email, password);
+    User nu = new User(nom, correu, contrasenya);
     em.persist(nu);
     return nu;
   }
@@ -69,7 +69,13 @@ public class UserService {
 
   public User getUserComplete(Long loggedId) {
     User u = getUser(loggedId);
-    u.getTasks().size();
+    u.getCompres().size();
+    u.getConverses().size();
+    u.getVendes().size();
+    u.getFavorits().size();
+    u.getMissatges().size();
+    u.getValoracions().size();
+    u.getProdVenda().size();
     return u;
   }
 }
