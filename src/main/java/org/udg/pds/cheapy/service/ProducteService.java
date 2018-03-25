82a6e875cfe2f3d3f383b466807fb72e71df4119
UngarMax <robriplopez@gmail.com>
2018-03-25 13:09:03 +0200

    Creat el servei Producte per la interacció amb la base de dades. Afegides algunes operacions bàsiques com buscar els Productes que estan en venda, buscar Producte per ID, crear Producte i esborrar Producte.

---
+++ src/main/java/org/udg/pds/cheapy/service/ProducteService.java
@@ -0,0 +1,88 @@
+package org.udg.pds.cheapy.service;
+
+import org.udg.pds.cheapy.model.Categoria;
+import org.udg.pds.cheapy.model.Producte;
+import org.udg.pds.cheapy.model.User;
+import org.udg.pds.cheapy.rest.RESTService;
+
+import javax.ejb.EJBException;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collection;
+
+@Stateless
+@LocalBean
+public class ProducteService
+{
+    @PersistenceContext
+    protected EntityManager em;
+
+    public Collection<Producte> getProductesEnVenda()
+    {
+        try
+        {
+            return em.createQuery("SELECT producte FROM Producte producte WHERE producte.transaccio IS NULL").getResultList();
+        }
+
+        catch (Exception ex)
+        {
+            // Very important: if you want that an exception reaches the EJB caller, you have to throw an EJBException
+            // We catch the normal exception and then transform it in a EJBException
+            throw new EJBException(ex);
+        }
+    }
+
+    public Producte get(Long id)
+    {
+        try
+        {
+            return em.find(Producte.class, id);
+        } catch (Exception ex)
+        {
+            // Very important: if you want that an exception reaches the EJB caller, you have to throw an EJBException
+            // We catch the normal exception and then transform it in a EJBException
+            throw new EJBException(ex);
+        }
+    }
+
+    public Producte crear(Categoria categoria, User venedor, String nom, Double preu, Boolean preuNegociable, Boolean intercanviAcceptat)
+    {
+        try
+        {
+            Producte producte = new Producte(categoria, venedor, nom, preu, preuNegociable, intercanviAcceptat);
+            em.persist(producte);
+            return producte;
+        }
+        catch (Exception ex)
+        {
+            // Very important: if you want that an exception reaches the EJB caller, you have to throw an EJBException
+            // We catch the normal exception and then transform it in a EJBException
+            throw new EJBException(ex);
+        }
+    }
+
+    public Producte crear(Categoria categoria, User venedor, String nom, Double preu, String descripcio, Boolean preuNegociable, Boolean intercanviAcceptat)
+    {
+        try
+        {
+            Producte producte = new Producte(categoria, venedor, nom, preu, descripcio, preuNegociable, intercanviAcceptat);
+            em.persist(producte);
+            return producte;
+        }
+        catch (Exception ex)
+        {
+            // Very important: if you want that an exception reaches the EJB caller, you have to throw an EJBException
+            // We catch the normal exception and then transform it in a EJBException
+            throw new EJBException(ex);
+        }
+    }
+
+    public RESTService.ID esborrar(Long idProducte)
+    {
+        Producte p = em.find(Producte.class, idProducte);
+        em.remove(p);
+        return new RESTService.ID(idProducte);
+    }
+}
