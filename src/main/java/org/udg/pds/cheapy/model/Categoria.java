220c712fac79edefb9578798f3a96c7d9b2e985d
UngarMax <robriplopez@gmail.com>
2018-03-25 12:51:29 +0200

    Entitat (model) Categoria creada.

---
+++ src/main/java/org/udg/pds/cheapy/model/Categoria.java
@@ -0,0 +1,52 @@
+package org.udg.pds.cheapy.model;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(Views.Private.class)
+    protected Long id;
+
+    @NotNull
+    @JsonView(Views.Public.class)
+    private String nom;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "categoria")
+    @JsonView(Views.Complete.class)
+    private Collection<Producte> productes;
+
+    public Categoria()
+    {
+        
+    }
+
+    public Categoria(String nom)
+    {
+        this.nom = nom;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getNom()
+    {
+        return nom;
+    }
+
+    public Collection<Producte> getProductes()
+    {
+        return productes;
+    }
+}
