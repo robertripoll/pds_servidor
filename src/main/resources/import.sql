06fe9ed77e72c604601cb7464ed246ea13ff8c35
UngarMax <robriplopez@gmail.com>
2018-04-11 13:43:31 +0200

    Canviat import.sql perquè es crein dades d'exemple: 1 producte, 1 categoria i 1 usuari.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,8 @@
 -- You can use this file to load seed data into the database using SQL statements
 insert into User (username, email, password) values ('jo', 'jp@hotmail.com', 'jo');
 insert into User (username, email, password) values ('tu', 'tu@hotmail.com', 'tu');
+
+-- Dades de mostra de Cheapy a inserir a la base de dades (1 usuari, 1 categoria, 1 producte)
+INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon) VALUES ('Benito', 'Camela', 'seatleon84@zmail.com', 'tetejohnny', '', 'masculí', '34612345678');
+INSERT INTO categoria (nom) VALUES ('Motor i Accessoris');
+INSERT INTO producte (dataPublicacio, intercanviAcceptat, nom, numVisites, preu, preuNegociable, reservat, categoria_id, venedor_id) VALUES ('', 1, 'Frens Brembo', 0, 87.90, 1, 0, 1, 1);
