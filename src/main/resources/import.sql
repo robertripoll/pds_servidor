431aa64a46a18458d253d8b18b9c65a7e8de0cb1
u1942303 <u1942303@campus.udg,edu>
2018-04-14 13:09:44 +0200

    Adaptat import.sql perquè s'afegeixi el producte de mostra a la taula que pertoca (degut al canvi de nom de la taula).

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,4 +5,4 @@ insert into User (username, email, password) values ('tu', 'tu@hotmail.com', 'tu
 -- Dades de mostra de Cheapy a inserir a la base de dades (1 usuari, 1 categoria, 1 producte)
 INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon) VALUES ('Benito', 'Camela', 'seatleon84@zmail.com', 'tetejohnny', STR_TO_DATE('11/04/2018', '%d/%m/%Y'), 'masculí', '34612345678');
 INSERT INTO categoria (nom) VALUES ('Motor i Accessoris');
-INSERT INTO producte (dataPublicacio, intercanviAcceptat, nom, numVisites, preu, preuNegociable, reservat, categoria_id, venedor_id) VALUES (STR_TO_DATE('11/04/2018', '%d/%m/%Y'), 1, 'Frens Brembo', 0, 87.90, 1, 0, 1, 1);
+INSERT INTO productes (dataPublicacio, intercanviAcceptat, nom, numVisites, preu, preuNegociable, reservat, categoria_id, venedor_id) VALUES (STR_TO_DATE('11/04/2018', '%d/%m/%Y'), 1, 'Frens Brembo', 0, 87.90, 1, 0, 1, 1);
