148b119dba50a9fbc5db4549aa7f93a58f92f4f7
u1942303 <u1942303@campus.udg,edu>
2018-04-19 21:20:22 +0200

    Creada entitat Missatge d'acord amb el diagrama de classes.

--- src/main/java/org/udg/pds/cheapy/model/Missatge.java
+++ src/main/java/org/udg/pds/cheapy/model/Missatge.java
@@ -1,19 +1,50 @@
 package org.udg.pds.cheapy.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.udg.pds.cheapy.rest.serializer.JsonDateDeserializer;
+import org.udg.pds.cheapy.rest.serializer.JsonDateSerializer;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Entity
 public class Missatge implements Serializable
 {
-    private static final long serialVersionUID = 1L;
+    public enum Estat {
+        PENDENT_ENVIAMENT("pendent"), ENVIAT("enviat"), LLEGIT("llegit");
 
-    public Missatge()
-    {
+        private String value;
 
+        private Estat(String value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public String getValue() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static Estat create(String val)
+        {
+            Estat[] estats = Estat.values();
+
+            for (Estat estat : estats)
+            {
+                if (estat.getValue().equalsIgnoreCase(val))
+                    return estat;
+            }
+
+            return PENDENT_ENVIAMENT;
+        }
     }
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +52,66 @@ public class Missatge implements Serializable
     protected Long id;
 
     @ManyToOne
-    private User usuari;
+    private User emisor;
 
+    @ManyToOne
+    private User receptor;
+
+    @Basic
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @JsonView(Views.Public.class)
+    private Estat estat;
+
+    @NotNull
+    private String missatge;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @JsonView(Views.Private.class)
+    @JsonSerialize(using = JsonDateSerializer.class)
+    @JsonDeserialize(using = JsonDateDeserializer.class)
+    private java.sql.Timestamp dataEnviament;
+
+    public Missatge()
+    {
+
+    }
+
+    public Missatge(User emisor, User receptor, String missatge)
+    {
+        this.emisor = emisor;
+        this.receptor = receptor;
+        this.missatge = missatge;
+        this.estat = Estat.PENDENT_ENVIAMENT;
+    }
+
+    public User getEmisor()
+    {
+        return emisor;
+    }
+
+    public User getReceptor()
+    {
+        return receptor;
+    }
+
+    public String getMissatge()
+    {
+        return missatge;
+    }
+
+    public Estat getEstat()
+    {
+        return estat;
+    }
+
+    public java.sql.Timestamp getDataEnviament()
+    {
+        return dataEnviament;
+    }
+
+    public void setEstat(Estat nouEstat)
+    {
+        estat = nouEstat;
+    }
 }
