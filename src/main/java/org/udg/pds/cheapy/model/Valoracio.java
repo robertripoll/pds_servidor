0a740f4e3c22fde445ffe3234b6adde3b8c5cb20
u1942303 <u1942303@campus.udg,edu>
2018-04-19 12:23:00 +0200

    "Creada" l'entitat Valoració amb els atributs corresponents a partir del diagrama de classes.

--- src/main/java/org/udg/pds/cheapy/model/Valoracio.java
+++ src/main/java/org/udg/pds/cheapy/model/Valoracio.java
@@ -1,25 +1,94 @@
 package org.udg.pds.cheapy.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Entity
 public class Valoracio implements Serializable
 {
-    private static final long serialVersionUID = 1L;
+    public enum Estrelles {
+        UNA(1), DUES(2), TRES(3), QUATRE(4), CINC(5);
 
-    public Valoracio()
-    {
+        private int value;
 
+        private Estrelles(int value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public int getValue() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static Estrelles create(int val)
+        {
+            Estrelles[] estrelles = Estrelles.values();
+
+            for (Estrelles actual : estrelles)
+            {
+                if (actual.getValue() == val)
+                    return actual;
+            }
+
+            return UNA;
+        }
     }
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonView(Views.Private.class)
     protected Long id;
 
+    @Basic
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull
+    @JsonView(Views.Public.class)
+    private Estrelles estrelles;
+
+    @JsonView(Views.Private.class)
+    private String comentaris;
+
     @ManyToOne
-    private User usuari_val;
+    private User valorador;
+
+    public Valoracio()
+    {
+
+    }
+
+    public Valoracio(User valorador, Estrelles estrelles)
+    {
+        this.valorador = valorador;
+        this.estrelles = estrelles;
+    }
+
+    public Valoracio(User valorador, Estrelles estrelles, String comentaris)
+    {
+        this(valorador, estrelles);
+
+        this.comentaris = comentaris;
+    }
+
+    public Estrelles getEstrelles()
+    {
+        return estrelles;
+    }
+
+    public String getComentaris()
+    {
+        return comentaris;
+    }
+
+    public User getValorador()
+    {
+        return valorador;
+    }
 }
