d2c1c61cba11f14be629baf02b708727bf1d87be
u1942303 <u1942303@campus.udg,edu>
2018-04-25 14:32:30 +0200

    Finalitzada tasca PDS-92. Ara es fa la creació de les Categories via JPA, no via l'import.sql S'han comentat els INSERT de l'import.sql S'ha creat el mètode create() de CategoriaService per poder crear Categories a través de CategoriaService.

--- src/main/java/org/udg/pds/cheapy/util/Global.java
+++ src/main/java/org/udg/pds/cheapy/util/Global.java
@@ -7,6 +7,8 @@ import javax.ejb.Startup;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
+import org.udg.pds.cheapy.model.Categoria;
+import org.udg.pds.cheapy.service.CategoriaService;
 import org.udg.pds.cheapy.service.UserService;
 
 @Singleton
@@ -19,10 +21,37 @@ public class Global
     @EJB
     UserService userService;
 
+    @EJB
+    CategoriaService categoriaService;
+
     @PostConstruct
     void init()
     {
-        // Aqui va la creació de dades de mostra
+        // Creació de Categories de mostra
+        Categoria c = new Categoria("Motor i Accessoris");
+        categoriaService.create(c);
+        c = new Categoria("Electrònica");
+        categoriaService.create(c);
+        c = new Categoria("Esport i Oci");
+        categoriaService.create(c);
+        c = new Categoria("Mobles, Decoració i Jardí");
+        categoriaService.create(c);
+        c = new Categoria("Consoles i Videojocs");
+        categoriaService.create(c);
+        c = new Categoria("Llibres, Pel·lícules i Música");
+        categoriaService.create(c);
+        c = new Categoria("Moda i Accessoris");
+        categoriaService.create(c);
+        c = new Categoria("Infantil");
+        categoriaService.create(c);
+        c = new Categoria("Immobiliària");
+        categoriaService.create(c);
+        c = new Categoria("Electrodomèstics");
+        categoriaService.create(c);
+        c = new Categoria("Serveis");
+        categoriaService.create(c);
+        c = new Categoria("Altres");
+        categoriaService.create(c);
 
         // Prova amb tots els tipus de llançar missatges al logger
         logger.fatal("Error fatal");
