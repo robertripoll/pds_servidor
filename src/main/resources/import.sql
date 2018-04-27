ab5a5d56889731401642c98f39c0ab5fd8cd1ddb
u1942303 <u1942303@campus.udg,edu>
2018-04-27 13:58:06 +0200

    Merge manual del fitxer import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
+SET foreign_key_checks = 0;
+
 -- Funcio de càlcul de distància
 
 DROP FUNCTION IF EXISTS distancia;
@@ -19,10 +21,15 @@ CREATE FUNCTION distancia
 
 -- DADES DE MOSTRA
 
+-- Ubicacions
+INSERT INTO ubicacions (id, ciutat, pais, coordLat, coordLng) VALUES (1, 'Girona', 'Catalunya', 41.9794005, 2.82142640);
+INSERT INTO ubicacions (id, ciutat, pais, coordLat, coordLng) VALUES (2, 'Bogotà', 'Colòmbia', 4.71098859, -74.072092);
+INSERT INTO ubicacions (id, ciutat, pais, coordLat, coordLng) VALUES (3, 'Washington DC', 'Estats Units', 38.9071923, -77.0368707);
+
 -- Usuaris
-INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon) VALUES ('Benito', 'Camela', 'seatleon84@zmail.com', 'tetejohnny', STR_TO_DATE('11/04/2018', '%d/%m/%Y'), 'HOME', '34612345678');
-INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon) VALUES ('Pablo Emilio', 'Escobar Gabiria', 'escobar@colombia.com', 'narcos33', STR_TO_DATE('03/05/1975', '%d/%m/%Y'), 'HOME', '54612345678');
-INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon) VALUES ('Donald', 'Trump', 'admin@whitehouse.gov', 'bigwall50', STR_TO_DATE('16/08/1954', '%d/%m/%Y'), 'HOME', '180000000');
+INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon, ubicacio_id) VALUES ('Benito', 'Camela', 'seatleon84@zmail.com', 'tetejohnny', STR_TO_DATE('11/04/2018', '%d/%m/%Y'), 'HOME', '34612345678', 1);
+INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon, ubicacio_id) VALUES ('Pablo Emilio', 'Escobar Gabiria', 'escobar@colombia.com', 'narcos33', STR_TO_DATE('03/05/1975', '%d/%m/%Y'), 'HOME', '54612345678', 2);
+INSERT INTO usuaris (nom, cognoms, correu, contrasenya, dataNaix, sexe, telefon, ubicacio_id) VALUES ('Donald', 'Trump', 'admin@whitehouse.gov', 'bigwall50', STR_TO_DATE('16/08/1954', '%d/%m/%Y'), 'HOME', '180000000', 3);
 
 -- Categories
 -- INSERT INTO categories (nom) VALUES ('Motor i Accessoris');
@@ -52,3 +59,5 @@ INSERT INTO missatge (estat, missatge, conversacio_id, emisor_id, receptor_id) V
 INSERT INTO missatge (estat, missatge, conversacio_id, emisor_id, receptor_id) VALUES ("ENVIAT", "Tinc ganes de muñeca hinchable", 2, 1, 2);
 INSERT INTO missatge (estat, missatge, conversacio_id, emisor_id, receptor_id) VALUES ("ENVIAT", "Gas al matalas", 1, 1, 1);
 INSERT INTO missatge (estat, missatge, conversacio_id, emisor_id, receptor_id) VALUES ("ENVIAT", "Gas al matalas", 2, 1, 2);
+
+SET foreign_key_checks = 1;
